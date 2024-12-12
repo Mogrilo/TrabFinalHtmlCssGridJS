65cca9763be64c8a80de5e2775b357fba782e130
Murilo Portela <160618849+Mogrilo@users.noreply.github.com>
2024-12-11 23:27:39 -0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,23 @@
+function abrirModalAlemanha(){
+    new bootstrap.Modal("#alemanha").show();
+}
+
+function abrirModalItalia(){
+    new bootstrap.Modal("#italia").show();
+}
+
+function abrirModalColombia(){
+    new bootstrap.Modal("#colombia").show();
+}
+
+function abrirModalPortugal(){
+    new bootstrap.Modal("#portugal").show();
+}
+
+function abrirModalFranca(){
+    new bootstrap.Modal("#franca").show();
+}
+
+function abrirModalGrecia(){
+    new bootstrap.Modal("#grecia").show();
+}
